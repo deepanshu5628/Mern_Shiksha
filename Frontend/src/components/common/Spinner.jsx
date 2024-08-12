2018452686a63a1ccff0dfbadc5a816864c28164
Deepanshu Yadav <130210093+deepanshu5628@users.noreply.github.com>
2024-08-12 23:54:48 +0530

    Delete Frontend/src directory

--- Frontend/src/components/common/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-function Spinner (){
-    return (
-        <div className="flex justify-center items-center relative max-h-full">
-            <div className="loader top-1/3"></div>
-        </div>
-    )
-}
-
-export default Spinner;
--- Frontend/src/components/core/AboutPage/ContactFormSection.jsx
+++
@@ -1,8 +0,0 @@
-
-function ContactFormSection(){
-    return (
-        <p>heelo world</p>
-    )
-}
-
-export default ContactFormSection;
